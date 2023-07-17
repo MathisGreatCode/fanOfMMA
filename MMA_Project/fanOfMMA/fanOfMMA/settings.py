050733c40110f046a9772eb782f8f4d4674170c5
MathisGreatCode <mathisvarcha@yahoo.fr>
2023-07-17 16:01:35 +0200

    navbar and profile changes

--- MMA_Project/fanOfMMA/fanOfMMA/settings.py
+++ MMA_Project/fanOfMMA/fanOfMMA/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +125,8 @@ STATIC_ROOT = '/MMA_Project/fanOfMMA/firstApp/static/'
 
 STATIC_URL = "static/"
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
